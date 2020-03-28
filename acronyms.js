81e28107bc8aa2ba038cecc9cf3948c5527a8bd7
William Daly <william.daly@bbc.co.uk>
2020-03-28 09:12:36 +0000

    Create acronyms.js

---
+++ acronyms.js
@@ -0,0 +1,13 @@
+// Techies love their TLA (Three Letter Acronyms)!
+
+// Help generate some jargonby writing a program that converts a long name like Portable Network Graphics 
+// to its acronym (PNG).
+
+function acroCreator(a,b,c)
+{
+	console.log(a[0] + b[0] + c[0])
+}
+
+acroCreator("Hello" , "World" , "Derp")
+
+console.log("-------------------------------------------------")
